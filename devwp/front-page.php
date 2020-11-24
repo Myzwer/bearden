58be39faff4664cd4ee7859862d1046e36ca7347
Myzwer <josh@onefortyfivedesign.com>
2020-11-24 09:20:54 -0500

    Knockout text header is added, though I will for sure have to fix it for mobile.

--- devwp/front-page.php
+++ devwp/front-page.php
@@ -13,11 +13,11 @@
 
 get_header(); ?>
 
-    <div class="grid-container full-width">
+<!--    <div class="grid-container full-width">
         <div class="grid-x grid-padding-x full-background" style = "background: linear-gradient(
-                    rgba(0, 0, 0, 0.3),
-                    rgba(0, 0, 0, 0.3)
-                    ),url(https://images.unsplash.com/photo-1591308627498-aac5dba0ebe2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80);
+                    rgba(0, 0, 0, 0.5),
+                    rgba(0, 0, 0, 0.5)
+                    ),url(https://knoxvilletn.gov/UserFiles/Servers/Server_109478/Image/Background/sunspherefall.jpg);
                     background-position: center center;
                     background-size: cover;">
             <div class="large-12 cell">
@@ -27,7 +27,30 @@ get_header(); ?>
                 </div>
             </div>
         </div>
+    </div>-->
+
+    <div class="full-width">
+        <div class="backdrop">
+            <p class="text multiply relative">
+                <span class="content-middle">Bearden</span>
+            </p>
+            <div class="content-middle">
+                <h4 class = "knockout-title">One Church. Two Locations.</h4>
+            </div>
+        </div>
     </div>
+
+    <div class="full-width main-background">
+        <div class = "grid-container">
+            <div class="grid-x grid-padding-x padding-outer">
+                <div class="small-12 center cell">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
 
 
 
