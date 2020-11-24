1f23c072e9e688551d74ecdaa320dd0c29ab5d0a
Myzwer <josh@onefortyfivedesign.com>
2020-11-24 15:15:51 -0500

    Finished the about us page until clients tell me what they want on the page. Added thumbnail support for the events page.

--- devwp/about.php
+++ devwp/about.php
@@ -14,17 +14,32 @@
 get_header(); ?>
 
 
-<div class="grid-container full-width">
+    <div class="grid-container full-width">
         <div class="grid-x grid-padding-x full-background" style = "background: linear-gradient(
                     rgba(0, 0, 0, 0.5),
                     rgba(0, 0, 0, 0.5)
-                    ),url(https://knoxvilletn.gov/UserFiles/Servers/Server_109478/Image/Background/sunspherefall.jpg);
+                    ),url(http://foothills-bearden.local/wp-content/uploads/2020/11/Bearden-About-Us.jpg);
                     background-position: center center;
                     background-size: cover;">
             <div class="large-12 cell">
                 <div class="content-middle">
-                    <h1 class = "center" >Foothills Bearden</h1>
+                    <h1 class = "center" >The Vision</h1>
                     <h4 class = "dark-color-invert">One Church. Two Locations.</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="full-width main-background">
+        <div class = "grid-container">
+            <div class="grid-x grid-padding-x padding-outer">
+                <div class="small-12 cell">
+                    <h3 class = "no-spacing add-padding no-bot-padding">The Vision Behind The Mission</h3>
+                    <p class = "no-top-padding">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae dolore dolores error exercitationem illo labore magnam modi non officiis omnis porro, quisquam sapiente sunt tempora vel velit voluptates voluptatum.</p>
+                    <p class = "no-top-padding">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae dolore dolores error exercitationem illo labore magnam modi non officiis omnis porro, quisquam sapiente sunt tempora vel velit voluptates voluptatum.</p>
+                    <p class = "no-top-padding">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae dolore dolores error exercitationem illo labore magnam modi non officiis omnis porro, quisquam sapiente sunt tempora vel velit voluptates voluptatum.</p>
+
+
                 </div>
             </div>
         </div>
