099e99fd4555aa79459fc77b0e63ee89c2dd6726
Myzwer <josh@onefortyfivedesign.com>
2020-12-08 11:58:06 -0500

    Continuing to work on the event pages, the main events page is now dynamic, and the structure for the subpages is finished.

--- devwp/home.php
+++ devwp/home.php
@@ -39,51 +39,53 @@ get_header(); ?>
         </div>
     </div>
 
-    <?php
-    if (have_posts()) :
-    while (have_posts()) : the_post();
-    ?>
+
 
     <div class="full-width main-background">
         <div class = "grid-container">
             <div class="grid-x grid-padding-x padding-outer">
+            <?php
+            if (have_posts()) :
+                while (have_posts()) : the_post();
+                ?>
                 <div class="small-12 medium-6 cell">
                     <div class="event-card">
                         <div class="event-card-upper">
-                            <img src="http://foothills-bearden.local/wp-content/uploads/2020/12/Bearden-Bible-Study.jpg" alt="">
+                            <img src="<?php the_post_thumbnail(); ?>" alt="">
                             <div class="event-times">
-                                <span class="date">January 24</span>
-                                <span class="time">5:00pm</span>
+                                <span class="date"><?php the_field( "date", $post->ID); ?></span>
+                                <span class="time"><?php the_field( "start_time", $post->ID); ?></span>
                             </div>
                         </div>
-                        <h2>Bearden Bible Study</h2>
+                        <h2><?php the_title(); ?></h2>
                         <div class="location">
-                            <p>Hilton Garden Inn</p>
-                            <p>6200 Papermill Dr NW</p>
-                            <p>Knoxville, TN 37919</p>
+                            <p><?php the_field( "location_name", $post->ID); ?></p>
+                            <?php the_field( "address", $post->ID); ?>
                         </div>
 
                         <?php
                         echo '<div class="registration-links btn-group">';
-                            echo '<button><a href="https://foothillschurch.churchcenter.com/registrations/events/653099">Register</a></button>';
-                            echo '<button>';
-                        echo '<a href="' . get_permalink() . '"> More Info</a>';
-                            echo '</button>';
+                        echo '<a href="';
+                        the_field("registration_link", $post->ID);
+                        echo '"><button class = "blue-button">Register</button></a>';
+                            echo '<a href="' . get_permalink() . '"><button> More Info</button></a>';
                         echo '</div>';
                         ?>
 
                     </div>
                 </div>
-            </div>
+
+            <?php
+            endwhile;
+            else:
+                _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+            endif;
+            ?>
         </div>
     </div>
+    </div>
 
-    <?php
-    endwhile;
-    else:
-    _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-    endif;
-    ?>
+
 
 
 
--- devwp/single.php
+++ devwp/single.php
@@ -11,9 +11,83 @@
 
 get_header(); ?>
 
+    <div class="grid-container full-width">
+        <div class="grid-x grid-padding-x full-background full-background-small" style = "background: linear-gradient(
+                    rgba(0, 0, 0, 0.5),
+                    rgba(0, 0, 0, 0.5)
+                    ),url(http://foothills-bearden.local/wp-content/uploads/2020/12/Bearden-Bible-Study.jpg);
+                    background-position: top center;
+                    background-size: cover;">
+            <div class="large-12 cell">
+                <div class="content-middle">
+                    <h1 class = "center" >Bearden Bible Study</h1>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="full-width main-background padding-bottom">
+        <div class = "grid-container ">
+            <div class="grid-x grid-padding-x grid-margin-x">
+                <div class="small-12 medium-6 large-4 cell info-card box-shadow">
+                    <div class="grid-x grid-padding-x">
+                        <div class="small-2 cell">
+                            <i class="far fa-calendar-alt"></i>
+                        </div>
+                        <div class="small-10 cell">
+                            <div class="card-content">
+                                <p>November 20 - November 20</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="small-12 medium-6 large-4 cell info-card box-shadow">
+                    <div class="grid-x grid-padding-x">
+                        <div class="small-2 cell">
+                            <i class="far fa-clock" id ="big-icon"></i>
+                        </div>
+                        <div class="small-10 cell">
+                            <div class="card-content">
+                                <p>7:00-9:00pm</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="small-12 medium-6 large-4 cell info-card-long box-shadow">
+                    <div class="grid-x grid-padding-x">
+                        <div class="small-2 cell">
+                            <i class="fas fa-map-marker-alt"></i>
+                        </div>
+                        <div class="small-10 cell">
+                            <div class="card-content">
+                                <p>6200 Papermill Dr NW</p>
+                                <p>Knoxville, TN 37919</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="full-width main-background padding-bottom">
+        <div class="grid-x grid-padding-x grid-margin-x">
+            <div class="small-12 medium-6 large-4 add-padding center">
+                <img class = "box-shadow rounded" src="http://foothills-bearden.local/wp-content/uploads/2020/12/Bearden-Bible-Study-Logo.png" alt="Promotional Stock Photo">
+            </div>
+            <div class="small-12 medium-6 large-8 cell">
+                <h1 class="light-color-invert">Details</h1>
+                <p class="no-spacing tb-padding">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consectetur
+                    consequuntur deserunt dolore doloremque eum, excepturi, illo laudantium libero optio qui suscipit
+                    tempora. Expedita id iusto nihil placeat repellendus sed.</p>
+                <div class="padding-top">
+                    <a href="#"><div class="button">Register</div></a>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 <?php
